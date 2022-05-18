549c202af71396a15cbd4046ef9ca0f06e6a5f1c
sgerodes <sgerodes@gmail.com>
2022-05-18 09:03:07 +0200

    add ebay scraper logic

--- src/scraper/ebay_kleinanzeigen/ebay_kleinanzeigen/spiders/ebay_rent_spider.py
+++ src/scraper/ebay_kleinanzeigen/ebay_kleinanzeigen/spiders/ebay_rent_spider.py
@@ -13,12 +13,13 @@ class EbayRentSpider(scrapy.Spider):
     ]
 
     def parse(self, response, **kwargs):
-        css_index_selector = '.aditem::attr(data-adid)'
+        #css_index_selector = '.aditem::attr(data-adid)'
+        css_index_selector = '.aditem'
         #xpath_selector = '//*[contains(concat( " ", @class, " " ), concat( " ", "aditem", " " ))]'
-        xpath_selector = "//*[contains(@class, 'aditem')]"
+        # xpath_selector = "//*[contains(@class, 'aditem')]"
 
-        #for elem in response.css(css_index_selector).getall():
-        for elem in response.xpath(xpath_selector):
+        for elem in response.css(css_index_selector):
+        #for elem in response.xpath(xpath_selector):
             item = EbayKleinanzeigenItem()
             item['source_id'] = elem.xpath("@data-adid").get() #.css('::attr(data-adid)')
             #  response.xpath("//*[contains(@class, 'aditem')]")[0].xpath("@data-adid").get()
